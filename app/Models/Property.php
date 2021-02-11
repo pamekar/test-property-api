28f3afa31349158932641315575c1e623578b730
pamekar <d.pamekar@gmail.com>
2021-02-12 00:40:29 +0100

    Decode json for property type

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Property extends Model
 {
     use HasFactory;
+
+    public function getPropertyTypeAttribute($value)
+    {
+        return json_decode($value, false);
+    }
 }
