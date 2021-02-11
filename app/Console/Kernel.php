62a2df84df34344e281b886925df722bd0e7d6c3
pamekar <d.pamekar@gmail.com>
2021-02-12 00:43:56 +0100

    Disable costly scheduled command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('updateAPIProperties')->everyFiveMinutes();
+        // $schedule->command('updateAPIProperties')->everyFiveMinutes();
     }
 
     /**
