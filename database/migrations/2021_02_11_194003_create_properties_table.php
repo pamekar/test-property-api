bb29a8c83a84a3cc0e30bcafcb7474053bfd92c3
pamekar <d.pamekar@gmail.com>
2021-02-11 21:26:10 +0100

    Implement migrations

--- database/migrations/2021_02_11_194003_create_properties_table.php
+++ database/migrations/2021_02_11_194003_create_properties_table.php
@@ -15,7 +15,25 @@ class CreatePropertiesTable extends Migration
     {
         Schema::create('properties', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger("api_property_type_id")->nullable();
+            $table->string("county", 191);
+            $table->string("country", 191);
+            $table->string("town", 191);
+            $table->text("description");
+            $table->text("address");
+            $table->text("image_full");
+            $table->text("image_thumbnail");
+            $table->text("latitude");
+            $table->text("longitude");
+            $table->integer("num_bedrooms");
+            $table->integer("num_bathrooms");
+            $table->integer("price");
+            $table->enum("type", ['rent', 'sale']);
+            $table->json("property_type")->nullable();
             $table->timestamps();
+
+            $table->index(['county', 'country', 'town', 'type']);
+
         });
     }
 
