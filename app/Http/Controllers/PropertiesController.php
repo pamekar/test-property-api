b04f756aee3fa5eff9832e16bfd0aaba08498639
pamekar <d.pamekar@gmail.com>
2021-02-12 08:05:03 +0100

    Search properties

--- app/Http/Controllers/PropertiesController.php
+++ app/Http/Controllers/PropertiesController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Property;
+use App\Models\PropertyType;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PropertiesController extends Controller
 {
@@ -24,9 +26,48 @@ class PropertiesController extends Controller
     }
 
     /**
+     * Display a listing of the resource.
+     *
+     * @param  Request  $request
+     *
+     * @return Application|Factory|View
+     */
+    public function search(Request $request)
+    {
+        $request->validate([
+            'search' => 'required|string|max:255',
+            'field'  => 'required|in:address,num_bedrooms,price,property_type,type'
+        ]);
+        $query = Property::query();
+
+        $search = $request->get('search');
+        $field = $request->get('field');
+        switch ($field) {
+            case 'address':
+                $query->where('address', 'like', "%{$search}%");
+                break;
+            case 'property_type':
+                $propertyTypes = PropertyType::query()->where('title', 'like', "%{$search}%")
+                    ->pluck('id')->toArray();
+                $query->whereIn('id', $propertyTypes);
+                break;
+            case 'num_bedrooms':
+            case 'price':
+            case 'type':
+                $query->where($field, $search);
+                break;
+        }
+
+        $properties = $query->paginate(15);
+        $properties->withPath("/properties/search?field={$field}&search={$search}");
+
+        return view('properties.index', compact('properties'));
+    }
+
+    /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Application|Factory|View
      */
     public function create()
     {
@@ -62,7 +103,7 @@ class PropertiesController extends Controller
      *
      * @param  int  $id
      *
-     * @return \Illuminate\Http\Response
+     * @return Application|Factory|View
      */
     public function edit($id)
     {
