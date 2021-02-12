38aa251602146e7f25e6d3868531a9c61e9f66d1
pamekar <d.pamekar@gmail.com>
2021-02-12 07:58:14 +0100

    Display error/success messages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,7 +52,21 @@
         </div>
     </nav>
 
-    <main class="py-4">
+    <main class="container py-4">
+        @if(session()->has('success'))
+            <div class="alert alert-success">
+                <p>{{session()->get('success')}}</p>
+            </div>
+        @endif
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
         @yield('content')
     </main>
 </div>
