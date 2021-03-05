6617b3afcb5b3f32a644ab57373f0120a5dba96e
pamekar <d.pamekar@gmail.com>
2021-03-05 12:44:55 +0100

    Default home page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PropertiesController;
+use App\Models\Property;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,7 +16,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $properties = Property::query()->paginate(15);
+
+    return view('properties.index', compact('properties'));
 });
 
 Route::get('properties/search', [PropertiesController::class, 'search'])->name('properties.search');
