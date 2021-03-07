a67971a410b4f9c1b70e92df37db095c75be2211
pamekar <d.pamekar@gmail.com>
2021-03-08 00:57:47 +0100

    Store new properties

--- app/Http/Controllers/PropertiesController.php
+++ app/Http/Controllers/PropertiesController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\SearchPropertiesRequest;
+use App\Http\Requests\StorePropertiesRequest;
+use App\Http\Requests\UpdatePropertiesRequest;
 use App\Models\Property;
 use App\Models\PropertyType;
 use Illuminate\Contracts\Foundation\Application;
@@ -11,6 +13,8 @@ use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Spatie\MediaLibrary\MediaCollections\Exceptions\FileDoesNotExist;
+use Spatie\MediaLibrary\MediaCollections\Exceptions\FileIsTooBig;
 
 class PropertiesController extends Controller
 {
@@ -62,12 +66,41 @@ class PropertiesController extends Controller
      * @param Request $request
      *
      * @return RedirectResponse
+     * @throws FileDoesNotExist
+     * @throws FileIsTooBig
      */
-    public function store(Request $request)
+    public function store(StorePropertiesRequest $request)
     {
+        // Find similar property, or create new one
+        $propertyType = PropertyType::query()
+            ->firstOrCreate(['title' => $request->get('property_type_title')],
+                ['description' => $request->get('property_type_description')]);
 
+        $property = new Property();
+        $property->property_type_id = $propertyType->id;
+        $property->county = $request->get('county');
+        $property->country = $request->get('country');
+        $property->town = $request->get('town');
+        $property->description = $request->get('description');
+        $property->address = $request->get('address');
+        $property->latitude = $request->get('latitude');
+        $property->longitude = $request->get('longitude');
+        $property->num_bedrooms = $request->get('num_bedrooms');
+        $property->num_bathrooms = $request->get('num_bathrooms');
+        $property->price = $request->get('price');
+        $property->type = $request->get('type');
 
-        return redirect()->route('properties.index')->with();
+        $property->save();
+
+        // Store image file
+        $property->addMedia($request->file('image'))->withResponsiveImages()->toMediaCollection('default_images');
+        $media = $property->getMedia('default_images')[0];
+        $property->image_full = $media->getUrl();
+        $property->image_thumbnail = collect($media->getResponsiveImageUrls())->last();
+
+        $property->save();
+
+        return redirect()->route('properties.index')->with(['success' => "New Property created"]);
     }
 
     /**
