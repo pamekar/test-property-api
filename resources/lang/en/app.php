ed1e88a2c7b149e8fa35c39577a64e0255740acc
pamekar <d.pamekar@gmail.com>
2021-03-08 00:56:01 +0100

    Add lang locale files

---
+++ resources/lang/en/app.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during application for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'county'                    => 'County',
+    'country'                   => 'Country',
+    'town'                      => 'Town',
+    'description'               => 'Description',
+    'address'                   => 'Address',
+    'image'                     => 'Image',
+    'image_full'                => 'Full Image',
+    'image_thumbnail'           => 'Image Thumbnail',
+    'latitude'                  => 'Latitude',
+    'longitude'                 => 'Longitude',
+    'num_bedrooms'              => 'Number of Bedrooms',
+    'num_bathrooms'             => 'Number of Bathrooms',
+    'price'                     => 'Price',
+    'type'                      => 'Type',
+    'property_type_title'       => 'Property Type Title',
+    'property_type_description' => 'Property Type Description'
+
+
+];
