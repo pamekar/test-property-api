f7ddce9f1005534671eab527788345489ce9f335
pamekar <d.pamekar@gmail.com>
2021-03-08 08:04:47 +0100

    API routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\PropertiesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('properties/search', [PropertiesController::class, 'search']);
+
+Route::resource('properties', PropertiesController::class);
